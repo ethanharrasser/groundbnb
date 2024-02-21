db63b043bca0bd614db1ec3594c9489e3b79e104
Ethan Harrasser <ethan.harrasser@gmail.com>
2024-02-21 13:24:08 -0800

    added error handling middleware

--- backend/app.js
+++ backend/app.js
@@ -5,27 +5,25 @@ const cors = require('cors');
 const csurf = require('csurf');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
+const routes = require('./routes');
+const { ValidationError } = require('sequelize');
+
 const { environment } = require('./config');
 const isProduction = environment === 'production';
-const routes = require('./routes');
 
 const app = express();
 
-// Connecting Middleware
-
+// Middleware
 app.use(morgan('dev'));
 app.use(cookieParser());
-
 if (!isProduction) {
     app.use(cors());
 }
-
 app.use(
     helmet.crossOriginResourcePolicy({
         policy: "cross-origin"
     })
 );
-
 app.use(
     csurf({
         cookie: {
@@ -39,6 +37,36 @@ app.use(
 // Routes
 app.use(routes)
 
+// Error Handlers
+app.use((_req, _res, next) => {
+    const err = new Error('The requested resource could not be found.');
+    err.title = 'Resource Not Found';
+    err.errors = { message: 'The requested resource could not be found.' };
+    err.status = 404;
+    next(err);
+});
 
+app.use((err, _req, _res, next) => {
+    if (err instanceof ValidationError) {
+        let errors = {};
+        for (let error of err.errors) {
+            errors[error.path] = error.message;
+        }
+        err.title = 'Validation Error';
+        err.errors = errors;
+    }
+    next(err);
+});
+
+app.use((err, _req, res, _next) => {
+    res.status(err.status || 500);
+    console.error(err);
+    res.json({
+        title: err.title || 'Internal Server Error',
+        message: err.message,
+        errors: err.errors,
+        stack: isProduction ? null : err.stack
+    });
+});
 
 module.exports = app;
