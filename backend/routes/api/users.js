f7445361d35c29639caa5b14d14c180e7e836434
Ethan Harrasser <ethan.harrasser@gmail.com>
2024-02-23 13:19:25 -0800

    added user input validation middleware to user signup backend endpoint

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,13 +1,36 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
+const { check } = require('express-validator');
 
+const { handleValidationErrors } = require('../../utils/validation.js');
 const { setTokenCookie, requireAuth } = require('../../utils/auth.js');
 const { User } = require('../../db/models');
 
 const router = express.Router();
 
+// Username, email, and password validation middleware
+const validateSignup = [
+    check('email')
+        .exists({ values: 'falsy' })
+        .isEmail()
+        .withMessage('Please provide a valid email address.'),
+    check('username')
+        .exists({ values: 'falsy' })
+        .isLength({ min: 4 })
+        .withMessage('Please provide a username with 4 or more characters.'),
+    check('username')
+        .not()
+        .isEmail()
+        .withMessage('Username cannot be an email address.'),
+    check('password')
+        .exists({ values: 'falsy' })
+        .isLength({ min: 6 })
+        .withMessage('Password must be 6 or more characters.'),
+    handleValidationErrors
+];
+
 // POST /api/users
-router.post('/', async (req, res) => {
+router.post('/', validateSignup, async (req, res) => {
     const { username, email, password } = req.body;
     const hashedPassword = bcrypt.hashSync(password);
 
@@ -20,7 +43,6 @@ router.post('/', async (req, res) => {
     };
 
     await setTokenCookie(res, safeUser);
-
     res.json({ user: safeUser });
 });
 
