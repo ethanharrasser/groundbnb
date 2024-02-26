4f3335603845b2a8d73b203f8037ed2ccbcb3bb2
Ethan Harrasser <ethan.harrasser@gmail.com>
2024-02-25 18:49:21 -0800

    added spot migration and model

---
+++ backend/db/models/spot.js
@@ -0,0 +1,54 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Spot extends Model {
+    static associate(models) {
+      Spot.belongsTo(models.User, {
+        foreignKey: 'ownerId'
+      });
+
+      Spot.hasMany(models.Review, {
+        foreignKey: 'spotId'
+      });
+
+      Spot.hasMany(models.Booking, {
+        foreignKey: 'spotId'
+      });
+
+      Spot.hasMany(models.SpotImage, {
+        foreignKey: 'spotId'
+      });
+    }
+  }
+  Spot.init({
+    ownerId: DataTypes.INTEGER,
+    address: DataTypes.STRING,
+    city: DataTypes.STRING,
+    state: DataTypes.STRING,
+    country: DataTypes.STRING,
+    lat: DataTypes.DECIMAL,
+    lng: DataTypes.DECIMAL,
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      validate: {
+        min: 0
+      }
+    },
+    createdAt: DataTypes.DATE,
+    updatedAt: DataTypes.DATE
+  }, {
+    sequelize,
+    modelName: 'Spot',
+  });
+  return Spot;
+};
