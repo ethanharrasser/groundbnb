388adf9047e513db3e684b322541be077b3c52a1
Ethan Harrasser <ethan.harrasser@gmail.com>
2024-02-26 10:56:46 -0800

    added spot images seeds

--- backend/db/seeders/20240226183417-demo-spot-images.js
+++ backend/db/seeders/20240226183417-demo-spot-images.js
@@ -10,23 +10,42 @@ if (process.env.NODE_ENV === 'production') {
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    await SpotImage.bulkCreate([
+      {
+        spotId: 1,
+        url: 'https://static.wikia.nocookie.net/elderscrolls/images/9/98/Breezerhome.jpg/revision/latest/scale-to-width-down/1000?cb=20111120111740',
+        preview: true
+      },
+      {
+        spotId: 2,
+        url: 'https://static.wikia.nocookie.net/elderscrolls/images/e/ed/Honeyside_view.png/revision/latest/scale-to-width-down/1000?cb=20121109235901',
+        preview: true
+      },
+      {
+        spotId: 3,
+        url: 'https://static.wikia.nocookie.net/elderscrolls/images/1/1a/Proudspire_Manor_Solitude.jpg/revision/latest/scale-to-width-down/1000?cb=20111113130716',
+        preview: false
+      },
+      {
+        spotId: 4,
+        url: 'https://static.wikia.nocookie.net/elderscrolls/images/7/76/Severin_Manor.png/revision/latest/scale-to-width-down/1000?cb=20130206192733'
+      },
+    ], {
+      valdate: true
+    });
   },
 
   async down (queryInterface, Sequelize) {
     options.tableName = 'SpotImages'
     const Op = Sequelize.Op
     return queryInterface.bulkDelete(options, {
-      name: {
-        [Op.in]: []
+      url: {
+        [Op.in]: [
+          'https://static.wikia.nocookie.net/elderscrolls/images/9/98/Breezerhome.jpg/revision/latest/scale-to-width-down/1000?cb=20111120111740',
+          'https://static.wikia.nocookie.net/elderscrolls/images/e/ed/Honeyside_view.png/revision/latest/scale-to-width-down/1000?cb=20121109235901',
+          'https://static.wikia.nocookie.net/elderscrolls/images/1/1a/Proudspire_Manor_Solitude.jpg/revision/latest/scale-to-width-down/1000?cb=20111113130716',
+          'https://static.wikia.nocookie.net/elderscrolls/images/7/76/Severin_Manor.png/revision/latest/scale-to-width-down/1000?cb=20130206192733'
+        ]
       }
     }, {});
   }
