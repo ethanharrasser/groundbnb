8453f3255363197a9bb5cbba7c0b40c3e6a70107
Ethan Harrasser <ethan.harrasser@gmail.com>
2024-02-26 12:51:04 -0800

    added review image seeds to seeder

--- backend/db/seeders/20240226183438-demo-review-images.js
+++ backend/db/seeders/20240226183438-demo-review-images.js
@@ -10,22 +10,29 @@ if (process.env.NODE_ENV === 'production') {
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    await ReviewImage.bulkCreate([
+      {
+        reviewId: 1,
+        url: 'https://images.uesp.net/thumb/6/66/SR-interior-Breezehome_07.jpg/200px-SR-interior-Breezehome_07.jpg'
+      },
+      {
+        reviewId: 3,
+        url: 'https://images.uesp.net/thumb/5/58/SR-interior-Honeyside_02.jpg/200px-SR-interior-Honeyside_02.jpg'
+      },
+      {
+        reviewId: 3,
+        url: 'https://images.uesp.net/thumb/e/ea/SR-interior-Honeyside.jpg/200px-SR-interior-Honeyside.jpg'
+      }
+    ], {
+      validate: true
+    });
   },
 
   async down (queryInterface, Sequelize) {
     options.tableName = 'ReviewImages'
     const Op = Sequelize.Op
     return queryInterface.bulkDelete(options, {
-      name: {
+      url: {
         [Op.in]: []
       }
     }, {});
