e55d38792047ad6bde26eca13e8d569fc1c948fc
Ethan Harrasser <ethan.harrasser@gmail.com>
2024-02-27 09:27:30 -0800

    DB HOTFIX - on delete cascading issues part 2, migration down functions

--- backend/db/migrations/20240226022501-create-spot.js
+++ backend/db/migrations/20240226022501-create-spot.js
@@ -66,6 +66,7 @@ module.exports = {
     }, options);
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Spots');
+    options.tableName = 'Spots';
+    await queryInterface.dropTable(options);
   }
 };
--- backend/db/migrations/20240226025214-create-booking.js
+++ backend/db/migrations/20240226025214-create-booking.js
@@ -54,6 +54,7 @@ module.exports = {
     }, options);
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Bookings');
+    options.tableName = 'Bookings';
+    await queryInterface.dropTable(options);
   }
 };
