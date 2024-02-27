c34c652859377917fde915a0e3d5bbf29e85478b
Ethan Harrasser <ethan.harrasser@gmail.com>
2024-02-27 11:52:28 -0800

    moved user signup validation middleware

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,41 +1,11 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
-const { check } = require('express-validator');
 
-const { handleValidationErrors } = require('../../utils/validation.js');
-const { setTokenCookie, requireAuth } = require('../../utils/auth.js');
+const { validateSignup } = require('../../utils/validation.js');
+const { setTokenCookie } = require('../../utils/auth.js');
 const { User } = require('../../db/models');
 
 const router = express.Router();
-
-// Username, email, and password validation middleware
-const validateSignup = [
-    check('firstName')
-        .exists({ values: 'falsy' })
-        .isLength({ min: 2 })
-        .withMessage('Please provide a first name with 2 or more characters.'),
-    check('lastName')
-        .exists({ values: 'falsy' })
-        .isLength({ min: 2 })
-        .withMessage('Please provide a last name with 2 or more characters.'),
-    check('email')
-        .exists({ values: 'falsy' })
-        .isEmail()
-        .withMessage('Please provide a valid email address.'),
-    check('username')
-        .exists({ values: 'falsy' })
-        .isLength({ min: 4 })
-        .withMessage('Please provide a username with 4 or more characters.'),
-    check('username')
-        .not()
-        .isEmail()
-        .withMessage('Username cannot be an email address.'),
-    check('password')
-        .exists({ values: 'falsy' })
-        .isLength({ min: 6 })
-        .withMessage('Password must be 6 or more characters.'),
-    handleValidationErrors
-];
 
 // POST /api/users
 router.post('/', validateSignup, async (req, res) => {
@@ -58,7 +28,7 @@ router.post('/', validateSignup, async (req, res) => {
         username: user.username
     };
 
-    await setTokenCookie(res, safeUser);
+    setTokenCookie(res, safeUser);
     res.json({ user: safeUser });
 });
 
--- backend/utils/validation.js
+++ backend/utils/validation.js
@@ -21,7 +21,7 @@ const handleValidationErrors = (req, _res, next) => {
     next();
 };
 
-// Spot validation middleware
+// Spot data validation middleware
 const validateSpot = [
     check('address')
         .exists({ values: 'falsy' })
@@ -60,7 +60,38 @@ const validateSpot = [
     handleValidationErrors
 ]
 
+// User signup username, email, and password validation middleware
+const validateSignup = [
+    check('firstName')
+        .exists({ values: 'falsy' })
+        .isLength({ min: 2 })
+        .withMessage('Please provide a first name with 2 or more characters.'),
+    check('lastName')
+        .exists({ values: 'falsy' })
+        .isLength({ min: 2 })
+        .withMessage('Please provide a last name with 2 or more characters.'),
+    check('email')
+        .exists({ values: 'falsy' })
+        .isEmail()
+        .withMessage('Please provide a valid email address.'),
+    check('username')
+        .exists({ values: 'falsy' })
+        .isLength({ min: 4 })
+        .withMessage('Please provide a username with 4 or more characters.'),
+    check('username')
+        .not()
+        .isEmail()
+        .withMessage('Username cannot be an email address.'),
+    check('password')
+        .exists({ values: 'falsy' })
+        .isLength({ min: 6 })
+        .withMessage('Password must be 6 or more characters.'),
+    handleValidationErrors
+];
+
+
 module.exports = {
     handleValidationErrors,
-    validateSpot
+    validateSpot,
+    validateSignup
 };
